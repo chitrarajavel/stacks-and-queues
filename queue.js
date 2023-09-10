c19d3518265e22b31d000650dc89b9f8eeb14596
Chitra Rajavel <chitrarajavel@gmail.com>
2023-09-10 15:55:50 -0500

    merge

--- queue.js
+++ queue.js
@@ -19,20 +19,49 @@ class Queue {
 
     /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-    enqueue(val) {}
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (this.first === null) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode; // Links Old node to the new node
+            this.last = newNode;
+        }
+        this.size++;
+    }
 
     /** dequeue(): remove the node from the start of the queue
      * and return its value. Should throw an error if the queue is empty. */
 
-    dequeue() {}
+    dequeue() {
+        if (this.size === 0) {
+            throw new Error('Empty Linked List');
+        }
+        let currentNode = this.first;
+        this.first = null;
+        this.first = currentNode.next;
+        this.size--;
+
+        // Check if the length of the linked list is empty after the values are removed and set the first and the last
+        if (this.size === 0) {
+            this.first = null;
+            this.last = null;
+        }
+        return currentNode.val;
+    }
 
     /** peek(): return the value of the first node in the queue. */
 
-    peek() {}
+    peek() {
+        return this.first.val;
+    }
 
     /** isEmpty(): return true if the queue is empty, otherwise false */
 
-    isEmpty() {}
+    isEmpty() {
+        return this.size === 0;
+    }
 }
 
 module.exports = Queue;
--- stack.js
+++ stack.js
@@ -1,46 +1,67 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+    /** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+    push(val) {
+        let newNode = new Node(val);
+        if (this.first === null) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            newNode.next = this.first; // Links new node to the first of the existing linked list
+            this.first = newNode;
+        }
+        this.size++;
+    }
 
-  }
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+    pop() {
+        if (this.size === 0) {
+            throw new Error('Empty Linked List');
+        }
+        let currentNode = this.first;
+        this.first = null;
+        this.first = currentNode.next;
+        this.size--;
 
-  pop() {
+        // Check if the size of the linked list is empty after the values are removed and set the first and the last
+        if (this.size === 0) {
+            this.first = null;
+            this.last = null;
+        }
+        return currentNode.val;
+    }
 
-  }
+    /** peek(): return the value of the first node in the stack. */
 
-  /** peek(): return the value of the first node in the stack. */
+    peek() {
+        return this.first.val;
+    }
 
-  peek() {
+    /** isEmpty(): return true if the stack is empty, otherwise false */
 
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+    isEmpty() {
+        return this.size === 0;
+    }
 }
 
 module.exports = Stack;
